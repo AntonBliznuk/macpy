d5aa00083e607f2340abc39a098ee66b25974b84
Антон <antonbliznuk71@gmail.com>
2025-10-04 23:59:12 +0100

    Add basic logic to manage dependecies.

---
+++ dependencies.py
@@ -0,0 +1,53 @@
+import subprocess
+
+
+def is_brew_installed() -> None:
+    try:
+        subprocess.run(
+            ["brew", "--version"],
+            check=True,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL
+        )
+        print(f"✅ Brew is installed.")
+    except subprocess.CalledProcessError as e:
+        raise Exception(f"❌ Brew is not instelled.\nInstall it here: https://brew.sh/")
+
+
+def is_dependencies_installed(dependencies: list[dict]) -> None:
+    for d in dependencies:
+        t = d.get("type")
+        n = d.get("name")
+        try:
+            subprocess.run(
+                ["brew", "list", f"--{t}", n],
+                check=True,
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL
+            )
+            print(f"✅ {n} is installed.")
+        except subprocess.CalledProcessError as e:
+            raise Exception(f"❌ {n} is not installed.\nInstall it with this command: brew install --{t} {n}")
+
+
+
+if __name__ == "__main__":
+    is_brew_installed()
+    is_dependencies_installed(
+        [
+            {
+                "type": "formula",
+                "name": "displayplacer"
+            },
+            {
+                "type": "formula",
+                "name": "blueutil"
+            },
+            {
+                "type": "cask",
+                "name": "cloudflare-warp"
+            },
+        ]
+    )
+
+
