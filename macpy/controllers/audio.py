e48010fefde82387680870c5dd00759adbcb4dda
Антон <antonbliznuk71@gmail.com>
2026-02-10 14:39:35 +0000

    Improved audio serializer

--- macpy/controllers/audio.py
+++ macpy/controllers/audio.py
@@ -3,13 +3,19 @@ from macpy.core import BaseController, CommandResult
 
 
 class AudioController(BaseController):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
-    @staticmethod
-    def set_volume(volume: int) -> CommandResult:
-        return AudioController._execute(
+        self.volume_sate: int|None = None
+
+    def set_volume(self, volume: int) -> CommandResult:
+        result = self._execute(
             [
                 "osascript",
                 "-e",
                 f"set volume output volume {volume}"
             ],
         )
+        if result.success:
+            self.volume_sate = volume
+        return result
